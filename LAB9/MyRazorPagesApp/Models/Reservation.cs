4f6de275275d727e3b27711812b3436b66626ad8
hamza1799 <hamzabasil74@gmail.com>
2024-05-01 14:18:15 +0300

    Added Reservation model and migration, created UML diagram

---
+++ LAB9/MyRazorPagesApp/Models/Reservation.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace YourProjectName.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; }
+      
+    }
+}
--- LAB9/MyRazorPagesApp/obj/Debug/net8.0/MyRazorPagesApp.AssemblyInfo.cs
+++ LAB9/MyRazorPagesApp/obj/Debug/net8.0/MyRazorPagesApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorPagesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017955b42f04ea0f964a7aac4466dedf59d8ae8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc42f28841a22dc3eabf7dff5869e1e6fbbaa377")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorPagesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorPagesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
